c68653f38eb24f5b6bbeaa64bb56abf9268f7f8d
Matt Chapman <matt@matthewchapman.co.uk>
2017-05-18 17:18:03 +0200

    some more R stuff

--- Experiments/R/ChangeDetection/reader.R
+++ Experiments/R/ChangeDetection/reader.R
@@ -1,9 +1,16 @@
 library(readr)
 library(changepoint)
+library(ggplot2)
 
+# read in the CSV
 data <- read_csv("~/shortexport.csv")
-x <- table(factor(format(data$postingdate, "%M")))
-changes <- cpt.mean(as.vector(x), method="BinSeg", test.stat = "CUSUM", penalty = "None", class = TRUE)
+# order the dataset by postingdate
+order(data$postingdate)
+# calculate factor, indexing on posting date 
+x <- table(factor(format(data$postingdate, "%m%d")))
+# detect changes, store results
+changes <- cpt.mean(as.vector(x), method="BinSeg", penalty = "MBIC" , class = TRUE, minseglen = 2)
+# plot dataset as line graph, draw vlines at change points
 plot(changes)
 lines(x, type="l")
 abline(v=changes@cpts, col="blue")
