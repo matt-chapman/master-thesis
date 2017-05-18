bfb2d5b03f7b8920caa0f3ec0963f00abd71aa40
Matt Chapman <matt@matthewchapman.co.uk>
2017-05-18 15:04:21 +0200

    eureka moment with R

--- R/ChangeDetection/reader.R
+++ R/ChangeDetection/reader.R
@@ -1,4 +1,9 @@
 library(readr)
+library(changepoint)
 
 data <- read_csv("~/shortexport.csv")
-table(factor(format(data$postingdate, "%D")))
+x <- table(factor(format(data$postingdate, "%M")))
+changes <- cpt.mean(as.vector(x), method="BinSeg", test.stat = "CUSUM", penalty = "None", class = TRUE)
+plot(changes)
+lines(x, type="l")
+abline(v=changes@cpts, col="blue")
