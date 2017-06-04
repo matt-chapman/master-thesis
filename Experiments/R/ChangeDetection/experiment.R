705c69d4a562855e9410b966b28d7e0512fbb528
Matt Chapman <matt@matthewchapman.co.uk>
2017-06-04 21:29:37 +0200

    Move todo package preamble to cls file. Make small changes to experiment.R, create pdf export of experiments on distributions

--- Experiments/R/ChangeDetection/experiment.R
+++ Experiments/R/ChangeDetection/experiment.R
@@ -70,7 +70,8 @@ RunExperiment <- function(input, daily=TRUE) {
   # Mean PELT
   mean.pelt <- cpt.mean(dataset$Freq,
                         method="PELT",
-                        penalty = "Hannan-Quinn"
+                        penalty = "Hannan-Quinn",
+                        minseglen = 5
                         )
   
   plot(mean.pelt, main="Mean w/PELT", ylab="Postings")
@@ -80,7 +81,8 @@ RunExperiment <- function(input, daily=TRUE) {
   # Mean SegNeigh
   mean.segneigh <- cpt.mean(dataset$Freq,
                             method="SegNeigh",
-                            penalty = "Hannan-Quinn")
+                            penalty = "Hannan-Quinn",
+                            Q=5)
   
   plot(mean.segneigh, main="Mean w/SegNeigh", ylab="Postings")
   abline(v=33, col="blue", lty=2)
@@ -90,7 +92,9 @@ RunExperiment <- function(input, daily=TRUE) {
   mean.binseg <- cpt.mean(dataset$Freq,
                           method="BinSeg",
                           test.stat = "CUSUM",
-                          penalty = "Hannan-Quinn")
+                          penalty = "Hannan-Quinn",
+                          minseglen = 5,
+                          Q=5)
   
   plot(mean.binseg, main="Mean w/BinSeg", ylab="Postings")
   abline(v=33, col="blue", lty=2)
@@ -109,7 +113,8 @@ RunExperiment <- function(input, daily=TRUE) {
   # Var SegNeigh
   var.segneigh <- cpt.var(dataset$Freq,
                           method="SegNeigh",
-                          penalty = "Hannan-Quinn")
+                          penalty = "Hannan-Quinn",
+                          Q=5)
   
   plot(var.segneigh, main="Variance w/SegNeigh", ylab="Postings")
   
@@ -118,7 +123,7 @@ RunExperiment <- function(input, daily=TRUE) {
                         method="BinSeg",
                         #test.stat = "CSS",
                         penalty = "Hannan-Quinn",
-                        Q=4)
+                        Q=5)
   
   plot(var.binseg, main="Variance w/BinSeg", ylab="Postings")
   abline(v=33, col="blue", lty=2)
@@ -138,7 +143,8 @@ RunExperiment <- function(input, daily=TRUE) {
   # MeanVar SegNeigh
   meanvar.segneigh <- cpt.meanvar(dataset$Freq,
                                   method="SegNeigh",
-                                  penalty = "Hannan-Quinn")
+                                  penalty = "Hannan-Quinn",
+                                  Q=5)
   
   plot(meanvar.segneigh, main="Mean & Variance w/SegNeigh", ylab="Postings")
   abline(v=33, col="blue", lty=2)
@@ -149,7 +155,7 @@ RunExperiment <- function(input, daily=TRUE) {
                                 method="BinSeg",
                                 test.stat = "Poisson",
                                 penalty = "Hannan-Quinn",
-                                Q=4)
+                                Q=5)
   
   plot(meanvar.binseg, main="Mean & Variance w/BinSeg", ylab="Postings")
   abline(v=33, col="blue", lty=2)
